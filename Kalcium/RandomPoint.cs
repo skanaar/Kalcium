03fd2a707af255dfe16973e8b52bfc1e6b8e6969
Daniel Kallin <daniel.kallin@gmail.com>
2011-03-05 08:08:25 +0100

    Added Point and Vector generators

---
+++ Kalcium/RandomPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Kalcium
+{
+    public static class RandomPoint
+    {
+        private static Random _rand = new Random();
+        public static Point Within(double w, double h)
+        {
+            return new Point(RandomNumber.Between(0, w), RandomNumber.Between(0, h));
+        }
+    }
+}
---
+++ Kalcium/UnitVector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Kalcium
+{
+    public static class UnitVector
+    {
+        public static Vector WithAngle(double angle)
+        {
+            return new Vector(Math.Cos(angle), Math.Sin(angle));
+        }
+    }
+}
